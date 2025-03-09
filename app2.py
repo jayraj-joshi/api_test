8f418cba9da0bd0d162a360db2218f2564c3a1e5
jayraj-joshi <166158847+jayraj-joshi@users.noreply.github.com>
2025-03-10 00:55:28 +0530

    added atomrank on cors

--- app2.py
+++ app2.py
@@ -38,7 +38,7 @@ app = FastAPI()
 # CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://atomprep.vercel.app"], 
+    allow_origins=["https://atomprep.vercel.app","https://atomrank.in/"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
